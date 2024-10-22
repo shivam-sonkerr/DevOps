0e8b25a9b0deeed746514dd52bbca6d36e10f37a
shivam-sonkerr <shivam87380@gmail.com>
2024-10-22 04:37:26 +0000

    Fixed Branches and script too

--- bash/learn.sh
+++ bash/learn.sh
@@ -7,3 +7,16 @@ if [ $a -lt $b ]
 then
    echo "a is less than b"
 fi
+
+
+c=$((a*b*a*b))
+echo "Value of c is $c "
+d=$((a*b*b))
+echo "Value of d is $d"
+echo $d
+if [ $c -lt $d ]
+then
+   echo "c is less than d"
+ else
+   echo "c is greater than d"
+fi
