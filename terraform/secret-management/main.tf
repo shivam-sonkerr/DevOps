65e43a4e5bfb5b1cbfcc9404c1dca507e5b01c57
shivam-sonkerr <shivam87380@gmail.com>
2024-12-11 11:37:10 +0530

    Using AWS KMS for encryption and also create a script for encrypt decrypt

--- terraform/secret-management/main.tf
+++ terraform/secret-management/main.tf
@@ -25,3 +25,27 @@ resource "aws_kms_alias" "cmk" {
   target_key_id = aws_kms_key.cmk.id
   name = "alias/kms-cmk"
 }
+
+
+data "aws_kms_secrets" "creds" {
+  secret {
+    name    = "db"
+    payload = file("${path.module}/db-creds.yml.encrypted")
+  }
+}
+
+locals {
+  db_creds = yamldecode(data.aws_kms_secrets.creds.plaintext["db"])
+}
+
+resource "aws_db_instance" "db_kms" {
+  allocated_storage = 5
+  db_name = "mydb"
+  instance_class = "db.t4g.micro"
+  engine = "mysql"
+  engine_version = "8.0.39"
+  skip_final_snapshot = true
+
+  username = local.db_creds.username
+  password = local.db_creds.password
+}
