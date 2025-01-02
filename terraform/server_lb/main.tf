25ab771fec02a6ce2378715a2106c436b53c4172
shivam-sonkerr <shivam87380@gmail.com>
2025-01-02 13:23:00 +0530

    Autoscaling Groups

--- terraform/server_lb/main.tf
+++ terraform/server_lb/main.tf
@@ -2,42 +2,81 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_instance" "server" {
-  ami = "ami-036841078a4b68e14"
-  instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.instance.id]
-
-user_data = <<-EOF
-            #!/bin/bash
-            echo "Hey there" > index.html
-            nohup busybox httpd -f -p 8080 &
-            EOF
-
-  user_data_replace_on_change = true
-
-  tags = {
-    Name = "Server"
-  }
-}
+# resource "aws_instance" "server" {
+#   ami = "ami-036841078a4b68e14"
+#   instance_type = "t2.micro"
+#   vpc_security_group_ids = [aws_security_group.instance.id]
+#
+# user_data = <<-EOF
+#             #!/bin/bash
+#             echo "Hey there" > index.html
+#             nohup busybox httpd -f -p ${var.server_port} &
+#             EOF
+#
+#   user_data_replace_on_change = true
+#
+#   tags = {
+#     Name = "Server"
+#   }
+# }
 
 
 resource "aws_security_group" "instance" {
   name = "tf-instance"
 
   ingress {
-    from_port = 8080
-    to_port = 8080
+    from_port = var.server_port
+    to_port = var.server_port
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
 
   }
 }
 
+resource "aws_launch_template" "launch" {
+  image_id      = "ami-036841078a4b68e14"
+  instance_type = "t2.micro"
 
-output "instance_ip_addr" {
-  value = aws_instance.server.private_ip
+  user_data = base64encode(<<-EOF
+    #!/bin/bash
+    echo "Hey there, how are you" > index.html
+    nohup busybox httpd -f -p ${var.server_port} &
+  EOF
+  )
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
-output "instance_public_ip_addr" {
-  value = aws_instance.server.public_ip
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+resource "aws_autoscaling_group" "launch" {
+
+  launch_template {
+    id      = aws_launch_template.launch.id
+    version = "$Latest"
+  }
+
+  vpc_zone_identifier = data.aws_subnets.default.ids
+
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key = "Name"
+    value = "launch_asg"
+    propagate_at_launch = true
+  }
 }
---
+++ terraform/server_lb/output.tf
@@ -0,0 +1,19 @@
+# output "instance_ip_addr" {
+#   value = aws_instance.server.private_ip
+# }
+#
+# output "instance_public_ip_addr" {
+#   value = aws_instance.server.public_ip
+# }
+#
+# output "public_dns" {
+#   value = aws_instance.server.public_dns
+# }
+#
+# output "subnet_id" {
+#   value = aws_instance.server.subnet_id
+# }
+#
+# output "security_group" {
+#   value = aws_security_group.instance.id
+# }
