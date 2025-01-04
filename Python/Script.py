072e522f7466f1b28556f837a666aaf18d312160
shivam-sonkerr <shivam87380@gmail.com>
2025-01-04 12:10:23 +0530

    EC2 boto3

--- Python/Script.py
+++ Python/Script.py
@@ -1,7 +1,24 @@
 import boto3
 
+
 s3 = boto3.resource('s3')
 
 
 for bucket in s3.buckets.all():
     print(bucket.name)
+
+
+ec2 = boto3.client('ec2')
+
+print ("EC2 Details: ")
+print ("\n")
+
+response = ec2.describe_instances()
+print(response)
+
+
+res = ec2.describe_vpcs()
+print ("\n")
+print ("VPC Details: ")
+
+print(res)
