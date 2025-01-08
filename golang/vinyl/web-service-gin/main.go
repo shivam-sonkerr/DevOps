c0d00b412561a78daa9a9763301d43af1df60df6
shivam-sonkerr <shivam87380@gmail.com>
2025-01-08 13:45:13 +0530

    REST API in Golang

--- golang/vinyl/web-service-gin/main.go
+++ golang/vinyl/web-service-gin/main.go
@@ -23,7 +23,7 @@ type album struct {
 // albums slice to seed record album data.
 var albums = []album{
 	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+	{ID: "2", Title: "Jerus", Artist: "Gerry Mulligan", Price: 17.99},
 	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
 }
 
