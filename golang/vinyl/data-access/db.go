8b3af7185b78886fffb372c1b84483680c64c122
shivam-sonkerr <shivam87380@gmail.com>
2025-01-12 10:45:12 +0530

    CRUD Operations done in MySQL using Golang

--- golang/vinyl/data-access/db.go
+++ golang/vinyl/data-access/db.go
@@ -7,7 +7,7 @@ import (
 	"log"
 )
 
-// Defining the data structure type STRUCT here to hold all the values.
+// Album Defining the data structure type STRUCT here to hold all the values.
 type Album struct {
 	ID     int64
 	Title  string
@@ -60,7 +60,8 @@ func getArtist(db *sql.DB) ([]Album, error) {
 	return albums, err
 }
 
-//Function to add a new value in the database and returning ID as integer value, note that primary key is ID and hence it is not defined as it should be unique
+//Function to add a new value in the database and returning ID as integer value,
+//note that primary key is ID and hence it is not defined as it should be unique.
 
 func addEntry(db *sql.DB, alb Album) (int64, error) {
 
@@ -77,6 +78,48 @@ func addEntry(db *sql.DB, alb Album) (int64, error) {
 		return 0, fmt.Errorf("addEntry: %v", err)
 	}
 	return id, nil
+}
+
+func updateAlbum(db *sql.DB, alb Album) error {
+	query := "UPDATE album SET title =?,artist = ?,price =?  WHERE id =?"
+
+	result, err := db.Exec(query, alb.Title, alb.Artist, alb.Price, alb.ID)
+
+	if err != nil {
+		return fmt.Errorf("updateAlbum: %v", err)
+	}
+
+	rowsAffected, err := result.RowsAffected()
+
+	if err != nil {
+		return fmt.Errorf("updateAlbum: %v", err)
+	}
+
+	if rowsAffected == 0 {
+		return fmt.Errorf("updateAlbum: no such album")
+	}
+	return nil
+}
+
+func deleteFromAlbum(db *sql.DB, id int) error {
+	query := `DELETE FROM album WHERE id = ?`
+
+	result, err := db.Exec(query, id)
+
+	if err != nil {
+		return fmt.Errorf("deletedFromAlbum: %v", err)
+	}
+
+	rowsAffected, err := result.RowsAffected()
+
+	if err != nil {
+		return fmt.Errorf("deletedFromAlbum: %v", err)
+	}
+
+	if rowsAffected == 0 {
+		return fmt.Errorf("deleteFromAlbum: no album found with id %d", id)
+	}
+	return nil
 }
 
 //Main function
@@ -101,7 +144,7 @@ func main() {
 	newAlbum := Album{
 		Title:  "Linkin Park",
 		Artist: "Mike",
-		Price:  25,
+		Price:  26,
 	}
 
 	//Adding a new value in the Database
@@ -111,6 +154,26 @@ func main() {
 		log.Fatal("Failed to add new album: ", err)
 	}
 	fmt.Printf("Successfully added new album with ID: %d\n", id)
+
+	albumtoUpdate := Album{
+		ID:     1,
+		Title:  "Unorthodox",
+		Artist: "Bruno Mars",
+		Price:  30.99,
+	}
+
+	err = updateAlbum(db, albumtoUpdate)
+
+	if err != nil {
+		fmt.Printf("Failed to update album:", err)
+	}
+
+	err = deleteFromAlbum(db, 18)
+	if err != nil {
+		log.Printf("Failed to Delete Album: %v", err)
+	} else {
+		fmt.Println("Successfully deleted album")
+	}
 
 	for _, album := range albums {
 		fmt.Printf("ID: %d, Title: %s, Artist: %s, Price: %.2f\n",
