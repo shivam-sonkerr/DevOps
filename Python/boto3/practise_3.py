61f733cb72df77a8bd3fba6e7982fb88274a1bf4
shivam-sonkerr <shivam87380@gmail.com>
2025-12-05 09:48:25 +0530

    Fixing the logic

--- Python/boto3/practise_3.py
+++ Python/boto3/practise_3.py
@@ -9,13 +9,31 @@ def batch_operation ():
 
     batch_instances = []
 
-    # print("Number of instances to be stopped: ")
-    # num = input()
-    # print("Provide instance IDs: ")
+    print("Number of instances to be stopped: ")
+    num = input()
+    if num == '0':
+        print("Invalid Input Zero, Exiting now!")
+        return
+    if not num.isnumeric() :
+        print("Entered a wrong count input. Exiting Now!")
+        return
 
-    # for i in range(0,int(num)):
-    #     i = input()
-    #     batch_instances.append(i)
+    print("Provide instance IDs: ")
+
+    for i in range(0,int(num)):
+        i = input()
+        instance_id = i.strip()
+        while instance_id == "":
+            print("Empty instances cannot be stopped, please provide proper instance ID")
+            i = input()
+            instance_id = i.strip()
+        batch_instances.append(instance_id)
+
+    if len(batch_instances) == 0:
+        print("No instances were provided by the user.")
+        return
+
+    print("Instances user wants to stop: ", batch_instances)
 
     current_running_list = []
 
@@ -27,10 +45,32 @@ def batch_operation ():
                 current_running_list.append(instance['InstanceId'])
 
 
-    print(current_running_list)
-    response = client.stop_instances(
-            InstanceIds = current_running_list
-            )
-    print("Instance has been stopped!")
+    print("Currently running instances are : ", current_running_list)
+
+    final_list = []
+
+    for l in batch_instances:
+        for m in current_running_list:
+            if l==m:
+                final_list.append(m)
+
+    if len(final_list) == 0:
+        print("No valid instances are there to be stopped")
+        return
+
+    print("Final list of instances that will be stopped: ",final_list)
+
+    print("Press y/n to continue further")
+
+    decision = input()
+
+    if decision in ('y','Y','yes'):
+        response = client.stop_instances(
+            InstanceIds= final_list
+        )
+        print("Instances have been stopped!")
+    else:
+        print("As you have selected no therefore none of the instances will be stopped")
+        return
 
 batch_operation()
