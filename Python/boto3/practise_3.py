eb4697b7a191ce9f394a254366e64afe6b83ae6b
shivam-sonkerr <shivam87380@gmail.com>
2025-12-15 09:36:35 +0530

    Parsed final AWS response for clearer message

--- Python/boto3/practise_3.py
+++ Python/boto3/practise_3.py
@@ -13,10 +13,13 @@ def batch_operation ():
         print(error.response['Error']['Code'])
         print(error.response['Error']['Message'])
         return
+
+
     batch_instances = []
 
     print("Number of instances to be stopped: ")
     num = input()
+
     if num == '0':
         print("Invalid Input Zero, Exiting now!")
         return
@@ -29,11 +32,13 @@ def batch_operation ():
     for i in range(0,int(num)):
         i = input()
         instance_id = i.strip()
-        while instance_id == "":
-            print("Empty instances cannot be stopped, please provide proper instance ID")
+        while instance_id == "" or not instance_id.startswith("i-") :
+            print("Empty instances or invalid instance ID given, please provide proper instance ID")
             i = input()
             instance_id = i.strip()
         batch_instances.append(instance_id)
+
+    batch_instances = list(dict.fromkeys(batch_instances))
 
     if len(batch_instances) == 0:
         print("No instances were provided by the user.")
@@ -50,15 +55,20 @@ def batch_operation ():
             if current_status == 'running':
                 current_running_list.append(instance['InstanceId'])
 
+            if current_status == 'stopped':
+                print("This instance is already stopped")
+
 
     print("Currently running instances are : ", current_running_list)
 
     final_list = []
 
-    for l in batch_instances:
-        for m in current_running_list:
-            if l==m:
-                final_list.append(m)
+    for batch_instance in batch_instances:
+        for running_instance in current_running_list:
+            if batch_instance == running_instance:
+                final_list.append(running_instance)
+        if batch_instance not in current_running_list:
+            print("This instance cannot be stopped as it is in different state than RUNNING",batch_instance)
 
 
     if len(final_list) == 0:
@@ -76,7 +86,19 @@ def batch_operation ():
             response = client.stop_instances(
                 InstanceIds= final_list
             )
-            print("Instances have been stopped!")
+            if 'StoppingInstances' in response:
+                for instance_status in response['StoppingInstances']:
+                    instance_id = instance_status['InstanceId']
+                    current_state = instance_status['CurrentState']['Name']
+                    previous_state = instance_status['PreviousState']['Name']
+
+                    print(f"Instance ID: {instance_id}")
+                    print(f"  Previous State: {previous_state}")
+                    print(f"  Current State: {current_state}")
+                    print("-" * 20)
+
+
+
         except ClientError as error:
                 print(error.response['Error']['Code'])
                 print(error.response['ResponseMetadata']['HTTPStatusCode'])
