23a6eedf3a163f9a5b638209f9323aa512358940
shivam-sonkerr <shivam87380@gmail.com>
2025-12-21 09:23:44 +0530

    Learn S3 via python boto3

---
+++ Python/boto3/s3.py
@@ -0,0 +1,63 @@
+import boto3
+from botocore.exceptions import ClientError
+from rich import print
+
+
+
+client = boto3.client('s3')
+
+print(f"Region: {client.meta.region_name}")
+
+print(f"Endpoint: {client.meta.endpoint_url}")
+
+def create_bucket():
+    try:
+        response = client.create_bucket(
+        Bucket = 'boto11321',
+            CreateBucketConfiguration = {
+                'LocationConstraint':'us-west-2'
+                # 'Location': {
+                #     'Name':'us-west-2'
+                # }
+            }
+    )
+    except ClientError as error:
+        print (error.response['Error']['Code'])
+        print(error.response['Error']['Message'])
+        print(error.response['ResponseMetadata']['HTTPStatusCode'])
+
+def list_buckets():
+    try:
+        response = client.list_buckets()
+        for bucket in response['Buckets']:
+            name = bucket['Name']
+            print("Name of the bucket are :",name)
+    except ClientError as error:
+        print(error.response['Error']['Code'])
+        print(error.response['Error']['Message'])
+        print(error.response['ResponseMetadata']['HTTPStatusCode'])
+
+
+def list_objects():
+
+    print("Enter bucket name: ")
+    bucket_name = input()
+
+    try:
+        response = client.list_objects(
+            Bucket = bucket_name
+        )
+
+        for content in response['Contents']:
+            file_name = content['Key']
+            file_size = content ['Size']
+            print("File name: ", file_name)
+            print("File Size", file_size)
+
+    except ClientError as error:
+        print(error.response['Error']['Code'])
+        print(error.response['Error']['Message'])
+
+create_bucket()
+list_buckets()
+list_objects()
